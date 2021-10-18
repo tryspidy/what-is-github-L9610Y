73b0a0c0c2570afb89c60e83eac4aabfd7f09575
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-18 15:13:11 +0530

    Initial commit

---
+++ main.sh
@@ -0,0 +1,6 @@
+GitHub, Inc. is a United States-based global company that provides hosting
+for software development version control using Git. In 2018, it became a
+subsidiary of Microsoft for US$7.5 billion.
+
+It offers the distributed version control and source code management
+functionality of Git, plus its own features.
